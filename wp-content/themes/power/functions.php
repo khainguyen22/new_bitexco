73aeeb990df485c24a9a57996bf7afa78b4387fe
Nguyen Khai 22 <nguyenvankhai161220@gmail.com>
2023-02-28 17:32:02 +0700

    update function

--- wp-content/themes/power/functions.php
+++ wp-content/themes/power/functions.php
@@ -1449,3 +1449,27 @@ function custom_login_redirect()
 
 
 add_filter('login_redirect', 'custom_login_redirect');
+// format phone number 
+function formatPhoneNumber($phone)
+{
+    $phone_check  = preg_replace('/[^0-9]/', '', $phone);
+    $country_code = '+84';
+    $phoneNumber = $country_code . substr($phone_check, 1);
+    if (strlen($phoneNumber) > 10) {
+        $countryCode = substr($phoneNumber, 0, strlen($phoneNumber) - 10);
+        $areaCode = substr($phoneNumber, -10, 3);
+        $nextThree = substr($phoneNumber, -7, 3);
+        $lastFour = substr($phoneNumber, -4, 4);
+        $phoneNumber = $countryCode . ' ' . $areaCode . ' ' . $nextThree . ' ' . $lastFour;
+    } else if (strlen($phoneNumber) == 10) {
+        $areaCode = substr($phoneNumber, 0, 3);
+        $nextThree = substr($phoneNumber, 3, 3);
+        $lastFour = substr($phoneNumber, 6, 4);
+        $phoneNumber = '(' . $areaCode . ') ' . $nextThree . ' ' . $lastFour;
+    } else if (strlen($phoneNumber) == 7) {
+        $nextThree = substr($phoneNumber, 0, 3);
+        $lastFour = substr($phoneNumber, 3, 4);
+        $phoneNumber = $nextThree . ' ' . $lastFour;
+    }
+    return $phoneNumber;
+}
